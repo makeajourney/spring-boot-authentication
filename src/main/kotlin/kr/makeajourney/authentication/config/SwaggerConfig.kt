a5439fed6c0c006ea4a62c51c6377c4fe3da8b06
Soyoun Kim <makeajourney@gmail.com>
2021-06-25 10:44:50 +0900

    swagger에 jwt token authoriaztion 적용

--- src/main/kotlin/kr/makeajourney/authentication/config/SwaggerConfig.kt
+++ src/main/kotlin/kr/makeajourney/authentication/config/SwaggerConfig.kt
@@ -5,7 +5,11 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiKey
+import springfox.documentation.service.AuthorizationScope
+import springfox.documentation.service.SecurityReference
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 
@@ -14,11 +18,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
 @EnableSwagger2
 class SwaggerConfig {
 
+    private fun apiKey(): ApiKey =
+        ApiKey("JWT", "Authorization", "header")
+
+    private fun securityContext(): SecurityContext =
+        SecurityContext.builder().securityReferences(defaultAuth()).build()
+
+    private fun defaultAuth(): List<SecurityReference> =
+        listOf(SecurityReference("JWT", arrayOf(AuthorizationScope("global", "accessEverything"))))
+
     @Bean
     fun api(): Docket =
         Docket(DocumentationType.SWAGGER_2)
+            .securityContexts(listOf(securityContext()))
+            .securitySchemes(listOf(apiKey()))
             .select()
-            .apis(RequestHandlerSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage("kr.makeajourney.authentication"))
             .paths(PathSelectors.any())
             .build()
 }
